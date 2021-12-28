3ed442c73e5e7f3e90d717a0f7918a8032283153
genebit <genebitara@gmail.com>
2021-12-28 11:33:02 +0800

    Updated GUI

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 from tkinter import font, ttk
 from tkinter.constants import HORIZONTAL
 from ttkthemes import ThemedTk
+import tkinter as tk
+import math
 
 # Fonts
 HEADING_FONT = 'Inter 14'
@@ -10,6 +12,19 @@ LABEL_FONT = 'Inter 11'
 WHITE = '#FFFFFF'
 
 def elements(window):
+    # Private values
+    video_file_path = tk.StringVar()
+    file_input_type = tk.StringVar()
+    output_file_path = tk.StringVar()
+    codec_value = tk.StringVar()
+    fps_value = tk.StringVar()
+
+    crf_value = tk.StringVar()
+    crf_value.set('28') # Default value
+
+    # -------------------------
+    # ELEMENTS ----------------
+    # -------------------------
     heading = ttk.Label(window, background=WHITE, text='Video Compressor', font=HEADING_FONT)
     heading.place(x=47, y=10)
 
@@ -36,25 +51,33 @@ def elements(window):
     ttk.Label(window, background=WHITE, text='Codec', font=LABEL_FONT).place(x=50, y=190)
 
     ttk.Style().configure('TCheckbutton', font=LABEL_FONT, background=WHITE)
-    ttk.Checkbutton(window, width=15, style='TCheckbutton', text='H264').place(x=50, y=210)
-    ttk.Checkbutton(window, width=15, style='TCheckbutton', text='H265').place(x=50, y=240)
+    ttk.Checkbutton(window, width=10, style='TCheckbutton', text='H264').place(x=50, y=210)
+    ttk.Checkbutton(window, width=10, style='TCheckbutton', text='H265').place(x=50, y=240)
 
     # -----FPS-----
-    ttk.Label(window, background=WHITE, text='Frame Rate', font=LABEL_FONT).place(x=220, y=190)
+    ttk.Label(window, background=WHITE, text='Frame Rate', font=LABEL_FONT).place(x=195, y=190)
 
-    fps_options = [24, 30, 60]
-    fps_option = ttk.Combobox(window, background=WHITE, values=fps_options, width=15, font=LABEL_FONT)
+    fps_options = [25, 30, 60]
+    fps_option = ttk.Combobox(window, background=WHITE, values=fps_options, width=17, font=LABEL_FONT)
     fps_option.set(fps_options[0])
-    fps_option.place(x=220, y=210)
+    fps_option.place(x=195, y=210)
 
     # -----CRF-----
     ttk.Style().configure('TScale', background=WHITE)
     ttk.Label(window, background=WHITE, text='Compression Rate', font=LABEL_FONT).place(x=400, y=190)
-    ttk.Scale(window, length=150, from_=0, to=51, orient=HORIZONTAL).place(x=400, y=210)
+
+    s = ttk.Scale(window, length=150, from_=0, to=51, orient=HORIZONTAL, command=lambda s:crf_value.set(math.floor(float(s))))
+    s.set(float(crf_value.get()))
+    s.place(x=400, y=210)
+
+    ttk.Label(window, background=WHITE, textvariable=crf_value, font=LABEL_FONT).place(x=550, y=208)
 
     # -----START BUTTON-----
     ttk.Style().configure('TButton', font=LABEL_FONT)
-    ttk.Button(window, text='Start Compression!').place(x=400, y=250)
+    ttk.Button(window, text='Start Compression!', command=start_compression).place(x=400, y=240)
+
+def start_compression():
+    pass
 
 def main():
     GUI = ThemedTk(theme='arc')
@@ -64,6 +87,5 @@ def main():
     GUI.geometry('600x300')
     GUI.mainloop()
     
-
 if __name__ == '__main__':
     main()
