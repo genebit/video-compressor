ad89265b64f4b50cb41d9e8e42bb467c1f8fc0e6
genebit <genebitara@gmail.com>
2022-01-01 17:56:09 +0800

    Finished multiple file compression, minor fixes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import math
 
 GREEN = '\033[92m'
 RESET = '\033[0m'
+YELLOW = '\033[93m'
 
 totaltime = []
 
@@ -26,9 +27,13 @@ def convertlisttostring(li):
 def checkforspace(path):
     character = list(path)
     for i in range(0, len(character)):
-        if character[i] == ' ' and character[i-1] != '\\':
-            character[i] = '\\ '
+        # set the char[i] to this if char == to '\', '(', ')' otherwise, just leave as it is
+        character[i] = '\\ 'if (character[i] == ' ') and character[i-1] != '\\' else character[i]
+        character[i] = '\(' if character[i] == '(' else character[i]
+        character[i] = '\)' if character[i] == ')' else character[i]
+            
     return convertlisttostring(character)
+
 
 def loader(directory):
     videos = open('videofiles.txt', 'w')
@@ -49,8 +54,9 @@ def multiplefilecompression(crf, fps, vcodec_opt):
     videos = open('videofiles.txt', 'r').read().splitlines()
     destdirs = open("outputfiles.txt", "r").read().splitlines()
 
-    CRF = '26' if crf == '' else crf
-    FPS = '25' if fps == '' else fps
+    # Set default values for crf fps and vcodec
+    CRF = 26 if crf == '' else crf
+    FPS = 25 if fps == '' else fps
     VCODEC_OPT = '1' if vcodec_opt == '' else vcodec_opt
 
     videocodec = ['libx264', 'libx265']
@@ -63,6 +69,12 @@ def multiplefilecompression(crf, fps, vcodec_opt):
         totaltime.append(time_elapsed)
 
 
+def createoutputdir(videopath):
+    outputfile = f'{videopath}/../Output'
+    if not os.path.isdir(outputfile):
+        os.system(f'mkdir {checkforspace(videopath)}/../Output')
+
+
 def main():
     logo = '''
     ▀█░█▀ ░▀░ █▀▀▄ █▀▀ █▀▀█ 　 █▀▀ █▀▀█ █▀▄▀█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ █▀▀█ █▀▀█ 
@@ -70,26 +82,32 @@ def main():
     ░░▀░░ ▀▀▀ ▀▀▀░ ▀▀▀ ▀▀▀▀ 　 ▀▀▀ ▀▀▀▀ ▀░░░▀ █▀▀▀ ▀░▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░▀▀
     '''
     print(logo)
-    videopath = input('[PROCESS] Video Path:\n> ')
-    crf = input('[PROCESS] Compression Rate [0-51] (Default: 26):\n> ')
-    fps = input('[PROCESS] Frames Per Second (Default: 25):\n> ')
-    videocodec = input('[PROCESS] Video Codec (H264/H265) [0/1] (Default: 1):\n> ')
+    videopath = input(f'{GREEN}[PROCESS] Video Path:{RESET}\n> ')
+    crf = input(f'{GREEN}[PROCESS] Compression Rate [0-51] (Default: 26):{RESET}\n> ')
+    fps = input(f'{GREEN}[PROCESS] Frames Per Second (Default: 25):{RESET}\n> ')
+    videocodec = input(f'{GREEN}[PROCESS] Video Codec (H264/H265) [0/1] (Default: 1):{RESET}\n> ')
 
     print(f'{GREEN}------------START COMPRESSION---------------{RESET}')
 
     loader(videopath)
-
-    outputfile = f'{videopath}/../Output'
-    if not os.path.isdir(outputfile):
-        os.system(f'mkdir {checkforspace(videopath)}/../Output')
-    
+    createoutputdir(videopath)
     multiplefilecompression(crf, fps, videocodec)
 
     print(f'{GREEN}----------------FINISHED!-------------------{RESET}')
-    print('Logs:')
+    print('LOGS:')
+    uncompressed_files = open('videofiles.txt', 'r').read().splitlines()
+    compressed_files = open("outputfiles.txt", "r").read().splitlines()
 
+    for i in range(0, len(uncompressed_files)):
+        original_filesize = converttomb(os.path.getsize(uncompressed_files[i]))
+        compressed_filesize = converttomb(os.path.getsize(compressed_files[i]))
+
+        print(
+            f'{GREEN}🗀  {uncompressed_files[i]}{RESET}',
+            f'\n↳{GREEN} 🗀  {compressed_files[i]}{RESET}',
+            f'\n{YELLOW}From {original_filesize} to {compressed_filesize}{RESET}',
+            f'\n{GREEN}Process Time: {round(totaltime[i]/60, 1)}min\n{RESET}')
 
 if __name__ == '__main__':
-    # TODO: Fix () not working
-    # TODO: Add logs after compression
+    # TODO: Solution for not using ../ for a cleaner log
     main()
